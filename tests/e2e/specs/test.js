60c8fa748051323ce47256ba1c3c34f38601eb7f
Andrew Hunter <andrewhunter1612@gmail.com>
2021-02-24 20:44:11 +0000

    e2e finished

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,75 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('operator click updates display with operation result', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('.display').should('contain', '7')
+  })
+
+  it('multiple operations chained together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number8').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '15')
+  })
+
+  it('positive numbers output', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '7')
+  })
+
+  it('negative numbers output', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-3')
+  })
+
+  it('decimal numbers output', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0.5')
+  })
+
+  it('massive numbers output', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number4').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '8000000000000')
+  })
+
+  it('divide by zero', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', 'Infinity')
+  })
+
 })
